8de6b1cc465c88f0fe8b913de1cda8544e868d56
Christian Egwim <emeksense@gmail.com>
2019-04-16 10:16:03 +0100

    Seed admin user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+admin = Admin.create! :email => 'emeksense@gmail.com', :password => 'password1', :password_confirmation => 'password1'
